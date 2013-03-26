5a59cfe48a3c98a184311773d575df44e5a50812
William McBrine <wmcbrine@gmail.com>
2013-03-25 20:11:07 -0400

    Ensure that Tkinter is used for the Mac app, even if Gtk is installed.

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ PLIST = {'CFBundleVersion': VER, 'CFBundleShortVersionString': VER,
          'CFBundleHelpBookName': 'Network Remote Help',
          'NSHumanReadableCopyright': '2008-2013 William McBrine'}
 OPTIONS = {'includes': 'Zeroconf', 'semi_standalone': True,
-           'site_packages': True, 'argv_inject': ['-g'], 'plist': PLIST,
-           'iconfile': 'Network Remote.icns'}
+           'site_packages': True, 'argv_inject': ['-g', '-t'],
+           'plist': PLIST, 'iconfile': 'Network Remote.icns'}
 
 setup(
     app=APP,
