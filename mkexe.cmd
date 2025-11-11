22f12c706c7a65f1a524cbdd93e16fec86d92a75
William McBrine <wmcbrine@gmail.com>
2025-11-11 14:53:11 -0500

    Old build stuff, including for Mac apps. Some of this will return later.

--- mkexe.cmd
+++
@@ -1,9 +0,0 @@
-rd /s /q build
-rd /s /q dist
-\python27-32\python setup-exe.py py2exe
-del dist\w9xpopen.exe
-rd /s /q dist\tcl\tk8.5\demos
-rd /s /q dist\tcl\tk8.5\images
-rd /s /q dist\tcl\tcl8.5\http1.0
-rd /s /q dist\tcl\tcl8.5\tzdata
-rd /s /q dist\tcl\tcl8.5\opt0.4
